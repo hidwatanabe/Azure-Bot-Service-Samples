dfa03ae7de06a9e23b22728da29561641e02d559
Hideo Takagi <hitakagi@microsoft.com>
2018-07-13 09:39:58 +0900

    modified

--- LuisQnaBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=hiderobot2-Web-Deploy.pubxml -p:Password=m1md5mXeFdRddCnMWm9reC7xgiLsd7g4TtyNixh78CTqLcGKFfn11gY42t3d
-
